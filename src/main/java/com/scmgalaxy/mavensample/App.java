207f1b56c71070917b64ee835e4841a2bf507873
root <root@ncdchefnode002.corp.ncodeit.com>
2018-07-10 22:54:37 +0530

    second message

--- src/main/java/com/scmgalaxy/mavensample/App.java
+++ src/main/java/com/scmgalaxy/mavensample/App.java
@@ -1,4 +1,4 @@
-package com.mycompany.app;
+package com.scmgalaxy.mavensample;
 
 /**
  * Hello world!
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World RefX! MAH-2--d" );
     }
 }
